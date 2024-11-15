3a165582c79f8b20ca481928bc63952da4312c99
Maryam Akoni <akoniodunayo@gmail.com>
2024-11-14 22:13:37 -0500

    Added Javascript for interactivity

---
+++ script.js
@@ -0,0 +1,83 @@
+document.addEventListener("DOMContentLoaded", function() {
+//form
+    const form = document.getElementById('contact-form');
+    const nameField = document.getElementById('name');
+    const emailField = document.getElementById('email');
+    const messageField = document.getElementById('message');
+
+    
+    const nameError = document.getElementById('name-error');
+    const emailError = document.getElementById('email-error');
+    const messageError = document.getElementById('message-error');
+
+    
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        nameError.style.display = 'none';
+        emailError.style.display = 'none';
+        messageError.style.display = 'none';
+
+        let valid = true;
+
+        if (nameField.value.trim() === '') {
+            nameError.textContent = 'Please enter a valid name';
+            nameError.style.display = 'block';
+            valid = false;
+        }
+
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        if (!emailPattern.test(emailField.value)) {
+            emailError.textContent = 'Please enter a valid email address.';
+            emailError.style.display = 'block';
+            valid = false;
+        }
+
+        if (messageField.value.trim() === '') {
+            messageError.textContent = 'Please enter your message';
+            messageError.style.display = 'block';
+            valid = false;
+        }
+
+        if (valid) {
+            console.log('Form submitted');
+            alert("Form submitted successfully!");
+        }
+    });
+});
+
+
+//details button
+
+const detailsBtn = document.getElementById('details-btn');
+const projectDetails = document.getElementById('project-details');
+
+detailsBtn.addEventListener('click', function() {
+    if (projectDetails.style.display === 'none') {
+        projectDetails.style.display = 'block';
+        detailsBtn.textContent = 'Hide Details'; 
+    } else {
+        projectDetails.style.display = 'none';
+        detailsBtn.textContent = 'Show Details'; 
+    }
+
+});
+
+
+//location
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(displayLocation, handleError);
+    } else {
+        document.getElementById("location").innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function displayLocation(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    document.getElementById("location").innerHTML = `Latitude: ${lat}°<br>Longitude: ${lon}°`;
+}
+
+document.getElementById("getLocationBtn").addEventListener("click", getLocation);
